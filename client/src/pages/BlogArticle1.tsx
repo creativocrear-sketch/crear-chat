2cc4c8633d83baeeb1e95509cb91367047b93832
creativocrear-sketch <creativocrear@gmail.com>
2026-04-23 11:28:06 -0500

    Fix HTML structure in BlogArticle1: add missing closing article tag

--- client/src/pages/BlogArticle1.tsx
+++ client/src/pages/BlogArticle1.tsx
@@ -500,6 +500,7 @@ export default function BlogArticle1() {
                   </div>
                 </div>
               </div>
+            </article>
             </div>
           </div>
         </section>
